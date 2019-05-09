07d41c4a1364a6868ed8ac864f7a84ca8ed6871c
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-05-09 13:10:56 -0700

    added simple color palette swap. Click on box with same color to erase

---
+++ painting.js
@@ -0,0 +1,36 @@
+
+var brushColor = 'white'
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+var paintBlock = document.querySelectorAll('.paintSquare')
+var paletteBlock = document.querySelectorAll('.paletteSquare')
+
+function canvasHandler(item) {
+  if(item.style.backgroundColor == 'white'){
+    item.style.backgroundColor = brushColor
+  } else if (item.style.backgroundColor == brushColor){
+    item.style.backgroundColor = 'white'
+  } else{
+    item.style.backgroundColor = brushColor
+  }
+
+}
+
+function paintBrushHandler(item){
+  brushColor = item.id // blue, red, etc...
+}
+
+for (let i = 0; i < paintBlock.length; i++) {
+  paintBlock[i].addEventListener('click', function() {
+    canvasHandler(paintBlock[i])
+  })
+}
+
+for (let i = 0; i < paletteBlock.length; i++) {
+  paletteBlock[i].addEventListener('click', function() {
+    paintBrushHandler(paletteBlock[i])
+  })
+}
+});
